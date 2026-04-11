dd0eefcffb51c60d9e730ae5cebd28597a09b352
vinaysoni1 <vinaysoni9174@gmail.com>
2026-04-11 20:28:15 +0530

    Varible and Data type

---
+++ class-6/index.py
@@ -0,0 +1,20 @@
+print("Varible and data type")
+a=complex(9,2)
+b=True
+c=None
+d="VINAY SONI"
+a1=12
+print(a)
+print(b)
+print(a+a1)
+print("The data type of a is", type(a))
+print("The data type of b is", type(b))
+print("The data type of c is", type(c))
+print("The data type of d is", type(d))
+
+
+list1 = [ 2,3,4,5, "Apple", "Banana", "Vinay", 6,7]
+print(list1)
+
+dic1 = {"Name": "Vinay soni", "Age": 19 ,"city": "Jabalpur"}
+print(dic1)
