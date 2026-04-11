82ba553f3a8cd54e4a31e419991982bb59171e4d
vinaysoni1 <vinaysoni9174@gmail.com>
2026-04-11 20:48:01 +0530

    calculator crete

---
+++ class-7/calculator.py
@@ -0,0 +1,30 @@
+# crete a calculator
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : "))
+
+# Addition
+sum = a+b
+print("The sum of a and b is:",sum)
+
+# Subtraction
+Sub = a-b
+print("The Subtraction of a and b is:",Sub)
+
+# multiply
+multiply = a*b
+print("The multiply of a and b is:",multiply)
+
+# division
+division= a/b
+print("The division of a and b is:",division)
+
+# Floor division
+Floordivision= a//b
+print("The Floor division of a and b is:",Floordivision)
+
+# module
+module= a%b
+print("The module of a and b is:",module)
+
+power=a**b
+print("The power of a and b is:",power)
---
+++ class-7/main.py
@@ -0,0 +1,8 @@
+# operator
+print(15+6)  #addition
+print(15-6)  #subtraction
+print(15*6)  #multiply 
+print(15/6)  #division
+print(15//6)  #floordivision
+print(15%6)   #module find remainder
+print(4**3)   #power multiply
